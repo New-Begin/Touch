d23f4bbe93026d367b49a8fd42d2fec2ac1dd1b0
yysnow <978510569@qq.com>
2014-09-09 21:01:21 +0800

    新增匹配Activity

--- gen/com/newbegin/touch/R.java
+++ gen/com/newbegin/touch/R.java
@@ -37,6 +37,7 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
+        public static final int activity_match=0x7f030001;
     }
     public static final class menu {
         public static final int login=0x7f080000;
