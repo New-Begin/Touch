27b6b964582f2d48fbf32695d5f6cca8d8a6a796
yysnow <978510569@qq.com>
2014-09-09 19:26:03 +0800

    get telephone

--- src/com/newbegin/touch/LoginActivity.java
+++ src/com/newbegin/touch/LoginActivity.java
@@ -1,16 +1,34 @@
 package com.newbegin.touch;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
+	private TelephonyManager telephonyManager;//电话号码管理器
+	private EditText telephone;//电话号码
+	private EditText password;//密码
+	private Button loginBtn;//登录按钮
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		telephone = (EditText) findViewById(R.id.telephone);
+		password = (EditText) findViewById(R.id.password);
+		loginBtn = (Button) findViewById(R.id.loginBtn);	
+
+		//获取本机电话号码
+		telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+		String phoneId = telephonyManager.getLine1Number();
+		telephone.setText(phoneId);	
 	}
 
 	@Override
