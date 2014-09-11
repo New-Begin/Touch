c71fe42224383f922a6c7b897480858682131b20
YingQi Hu <mr.hoo7793@gmail.com>
2014-09-11 10:12:28 +0800

    在com.newbegin.touch.utils包下创建statusCode类，定义全局统一的返回状态码及标识符。程序中所用的常量全用此类中的属性代表，新定义的标识，以public final static 类型写入此类。

---
+++ src/com/newbegin/touch/utils/StatusCode.java
@@ -0,0 +1,15 @@
+package com.newbegin.touch.utils;
+
+public class StatusCode {
+	
+	//状态码，用于标识函数执行结果
+	public static final int SUCCESS = 0;
+	public static final int EMAIL_ALREADY_REGISTED = 1;
+	public static final int USER_ALREADY_REGISTED = 2;
+	public static final int PWD_ERROR = 3;
+	public static final int ID_NOT_EXSIT = 4;
+	 
+	//动作名，用于标识客户端传输数据的意义
+	public static final String LOGIN = "login";
+
+}
